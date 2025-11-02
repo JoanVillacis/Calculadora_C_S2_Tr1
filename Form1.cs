a4bbed123da049562aa4d989551737c369a7dbf7
Hamilton Villacis <joanss352@gmail.com>
2025-11-02 12:45:53 -0500

    Interfaz de la calculadora

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+namespace Calcularoa_C_S2_Tr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
