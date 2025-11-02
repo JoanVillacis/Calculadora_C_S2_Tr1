83e7538a07ab727b88015a095bc9c17506428da1
Hamilton Villacis <joanss352@gmail.com>
2025-11-02 15:05:56 -0500

    Se agrego las funciones de los botones

---
+++ Vista/Form1.cs
@@ -0,0 +1,115 @@
+namespace Calcularoa_C_S2_Tr1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btn_Siete_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("7");
+        }
+
+        private void btn_Ocho_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("8");
+        }
+
+        private void btn_Nueve_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("9");
+        }
+
+        private void btn_Cuatro_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("4");
+        }
+
+        private void btn_Cinco_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("5");
+        }
+
+        private void btn_Seis_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("6");
+        }
+
+        private void btn_Uno_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("1");
+        }
+
+        private void btn_Dos_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("2");
+        }
+
+        private void btn_Tres_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("3");
+        }
+
+        private void btn_Cero_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("0");
+        }
+
+        private void btn_Punto_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText(".");
+        }
+
+        private void btn_Igual_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("=");
+        }
+
+        private void btn_Suma_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("+");
+        }
+
+        private void btn_Resta_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("-");
+        }
+
+        private void btn_Multi_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("*");
+        }
+
+        private void btn_Div_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("/");
+        }
+
+        private void btn_Porcentaje_Click(object sender, EventArgs e)
+        {
+            txt_pantalla.AppendText("%");
+        }
+    }
+}
